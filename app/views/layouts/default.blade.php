b99217910ffae6080eb56fc7c27629d040740768
Pasindu Dissanayaka <pasindu@pasindudissan.xyz>
2025-07-03 19:17:45 +0530

    Frontend for Dashboard Pages

---
+++ app/views/layouts/default.blade.php
@@ -0,0 +1,45 @@
+{{-- Default Layout --}}
+<!DOCTYPE html>
+<html lang="en">
+@include('partials/head')
+
+<body class="g-sidenav-show bg-gray-100">
+    {{-- Side Nav Bar --}}
+    @include('partials/side-menu')
+    {{-- end Side Nav Bar --}}
+    {{-- Main Container --}}
+    <div class="main-content position-relative max-height-vh-100 h-100">
+        {{-- Navbar --}}
+        @include('partials/top-menu')
+        {{-- End Navbar --}}
+        @yeild('content')
+
+        <footer class="footer py-4  ">
+            <div class="container-fluid">
+                <div class="row align-items-center justify-content-lg-between">
+                    <div class="col-lg-6 mb-lg-0 mb-4">
+                        <div class="copyright text-center text-sm text-white text-lg-start">
+                            © <script>
+                                document.write(new Date().getFullYear())
+                            </script>,
+                            made with <i class="fa fa-heart" aria-hidden="true"></i> by
+                            <a href="https://www.pasindudissan.xyz" class="font-weight-bold text-white" target="_blank">Pasindu Dissanayaka</a>
+                            for Aspirations I-Lab.
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6">
+                        <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                            <li class="nav-item">
+                                <a href="https://www.pasindudissan.xyz" class="nav-link text-white" target="_blank">Pasindu Dissanayaka</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    {{-- end main container --}}
+    @include('partials/footer')
+</body>
+
+</html>
